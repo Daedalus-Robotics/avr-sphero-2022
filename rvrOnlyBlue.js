74b90fb63c1d7339d076d4020ba1138c1f8dffbd
Sorion55 <117539232+Sorion55@users.noreply.github.com>
2022-11-19 10:00:00 -0500

    Update rvrOnlyBlue.js

--- rvrOnlyBlue.js
+++ rvrOnlyBlue.js
@@ -1,52 +1,62 @@
-var lengthToWater =214;
+var lengthToWater =187;
+var n = 0;
 async function startProgram() {
 	//await speak("Blue",false);
 	
 	
 	
-	await driveToDistance(0,80,406);
+	await driveToDistance(0,80,436);
 	
 	stopRoll();
-	await roll(0,50,1.75);
+	//await roll(0,50,1.75);
 	await delay(1);
 	setHeading(270);
 
 
-	await driveToDistance(270, 80, (lengthToWater-15));
+	await driveToDistance(270, 80, lengthToWater);
 	stopRoll();
 
 	//water tower knocking
-	setHeading(90);
-	await roll(0, -100, 1.5 );
-	await roll(0, 25,3);
+	await driveToDistance(0, -70, 66);
+	while (n<12){
+		
+		await driveToDistance(0, 25,25);
+		await driveToDistance(0,-50,25);
+		n++;
+	}
+	await driveToDistance(0,25,66);
+	
 	await driveToDistance(270,50,lengthToWater);
-	setHeaing(180);
-	await roll(180,40,1);
+	setHeading(180);
+	
 	resetAim();
 	
+	//rolling down hill to other side
+	await driveToDistance(0,80, 685);
+	await driveToDistance(0,80,436);
 	
-	await driveToDistance(0,80, 1069);
 	
-	stopRoll();
-	await roll(0,50,1.75);
-	await delay(1);
-	setHeading(270);
-
-
-	await driveToDistance(270, 80, (lengthToWater-15));
+	await driveToDistance(270, 80, lengthToWater);
 	stopRoll();
 
 	//water tower knocking
-	setHeading(90);
-	await roll(0, -100, 1.5 );
-	await roll(0, 25,3);
+	await driveToDistance(0, -70, 66);
+	while (n<12){
+		
+		await driveToDistance(0, 25,25);
+		await driveToDistance(0,-50,25);
+		n++;
+	}
+	await driveToDistance(0,25,66);
+	
 	await driveToDistance(270,50,lengthToWater);
-	setHeaing(180);
-	await roll(180,40,1);
+	setHeading(180);
+	
 	resetAim();
 	
-	await driveToDistance(0,80,533);
-	await driveToDistance(270, 40,161);
+	
+	
+	
 	
 			
 }
