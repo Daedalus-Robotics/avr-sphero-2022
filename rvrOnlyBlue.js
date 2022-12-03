e1f2e9e2d62382d8fdf21864f011829d1bb17586
Sorion55 <117539232+Sorion55@users.noreply.github.com>
2022-12-03 10:56:25 -0600

    Update rvrOnlyBlue.js

--- rvrOnlyBlue.js
+++ rvrOnlyBlue.js
@@ -1,12 +1,13 @@
 var lengthToWater =190;
 var n = 0;
 var distanceUpFirstRamp = 480;
+var distanceToFire = 520;
 async function startProgram() {
 	//await speak("Blue",false);
 	
 	
 	
-	await driveToDistance(0,100,distanceUpFirstRamp);
+	await driveToDistance(0,255,distanceUpFirstRamp);
 	
 	stopRoll();
 	//await roll(0,50,1.75);
@@ -14,26 +15,26 @@ async function startProgram() {
 	setHeading(270);
 
 
-	await driveToDistance(270, 80, lengthToWater);
+	await driveToDistance(270, 200, lengthToWater);
 	stopRoll();
 
 	//water tower knocking
 	await driveToDistance(0, -70, 66);
 	while (n<12){
 		
-		await driveToDistance(0, 100,25);
+		await driveToDistance(0, 200,25);
 		await driveToDistance(0,-70,40);
 		n++;
 	}
-	await driveToDistance(0,25,66);
+	await driveToDistance(0,100,66);
 	
-	await driveToDistance(270,50,lengthToWater);
+	await driveToDistance(270,200,lengthToWater);
 	setHeading(185);
 	await delay(2);
 	resetAim();
 	
 	//rolling down hill to other side
-	await driveToDistance(0,100, 685);
+	/*await driveToDistance(0,100, 685);
 	
 	await driveToDistance(0,100,630);
 	
@@ -52,13 +53,13 @@ async function startProgram() {
 	await driveToDistance(0,25,66);
 	
 	await driveToDistance(270,50,lengthToWater);
-	setHeading(180);
-	await delay(2);
-	/*resetAim();
+	*/
 	
-	await driveToDistance(0,80,530);
+	//Autonomous Parking
+	
+	await driveToDistance(0,80,distanceToFire);
 	await driveToDistance(270,80,188);
-	
+	/*
 	
 	await delay(4);
 	
@@ -76,7 +77,7 @@ async function startProgram() {
 		
 		await delay(0.025);
 	*/
-	}
+	
 	
 	
 	
